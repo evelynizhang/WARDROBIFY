ba5be10fb542e87ed89d3d117476b1294922d024
Eluron <elurono@gmail.com>
2023-07-19 14:33:49 -0500

    Added BinVO

--- hats/api/hats_rest/models.py
+++ hats/api/hats_rest/models.py
@@ -19,13 +19,3 @@ class Hat(models.Model):
 
     def __str__(self):
         return self.style_name
-
-class Hat(models.Model):
-    fabric = models.CharField(max_length=100)
-    style_name = models.CharField(max_length=100)
-    color = models.CharField(max_length=50)
-    picture_url = models.URLField()
-    wardrobe_location = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.style_name
