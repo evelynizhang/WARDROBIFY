ad586ca2ebaea22add47e3565c1d716e651dfe28
Eluron <elurono@gmail.com>
2023-07-19 13:53:27 -0500

    New Hat models with FK

--- hats/api/hats_rest/models.py
+++ hats/api/hats_rest/models.py
@@ -1,3 +1,22 @@
+# hats/api/hats_rest/models.py
+
 from django.db import models
 
-# Create your models here.
+
+class LocationVO(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Hat(models.Model):
+    fabric = models.CharField(max_length=100)
+    style_name = models.CharField(max_length=100)
+    color = models.CharField(max_length=50)
+    picture_url = models.URLField()
+    wardrobe_location = models.ForeignKey(LocationVO, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.style_name
