7fd92dd94b2258382a530476942b6838291445bf
Ni <evelyn.z.mccoy@gmail.com>
2023-07-19 10:10:04 -0700

    created shoe model

--- shoes/api/shoes_rest/models.py
+++ shoes/api/shoes_rest/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Shoe(models.Model):
+    brand = models.CharField(max_length=200)
+    size = models.PositiveSmallIntegerField()
+    color = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.brand
